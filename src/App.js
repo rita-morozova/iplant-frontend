a3f1840bfb316e24d4b0d929a0f517f78c3b1b4e
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-06 15:23:55 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import PlantsContainer from './Containers/PlantsContainer'
 import FavoritesContainer from './Containers/FavoritesContainer'
 import Cart from './Components/Cart'
 import Navbar from './Components/Navbar'
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-function App() {
+class App extends React.Component {
+
+  render(){
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Navbar />
+        <div>
+          <Route exact path='/' component={PlantsContainer} />
+          <Route exact path='/all-plants' component={PlantsContainer} />
+          <Route exact path='/my-picks' component={FavoritesContainer} />
+          <Route exact path='/my-cart' component={Cart} />
+        </div>
+      </Router>
+
   
     </div>
   );
+  }
 }
 
 export default App;
