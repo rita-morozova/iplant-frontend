6143bd9d0fba2e8a22256c3e4e0ca2a555d950bf
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-08 20:45:53 -0800

    add 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import notFound from '../images/notFound.png'
+import cat from '../images/cat.jpg'
 
 const NotFound = () => {
   
   return(
     <div>
       <br />
-      404 PAGE NOT FOUND
+      <h1> 404 </h1>
+      <h2>Sorry, This Page Is Not Found</h2>
+      <br />
+      <img src={cat}/>
     </div>
     )}
 
