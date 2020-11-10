ac4d1c9270a17b6d9b83f6a191fb19fb2c1ae7f3
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-10 10:39:59 -0800

    add calculate cart total

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import PlantCard from './PlantCard'
 import {Link} from 'react-router-dom'
 import './Cart.css'
+import CartTotal from './CartTotal'
+import {Icon} from 'semantic-ui-react'
+
 
 
 
 class Cart extends React.Component {
 
+  // state ={
+  //   qty: 1,
+  // }
+
+  // getQty = e =>{
+  //   this.setState({
+  //     qty: e.target.value,
+  //   })
+  // }
+
 
   render(){
     const {cart} = this.props
-    console.log(cart)
+    
     return(
       <div>
         <br />
@@ -18,37 +30,31 @@ class Cart extends React.Component {
         <div>
           {cart.map(plant => (
             <li className='cart-item' key={plant.id}>
-              <img className='cart-item-image' src={plant.plant.image} />
+              <img className='cart-item-image' src={plant.plant.image} alt='plant' />
               <div className='cart-item-info'>
               <h1 className='cart-item-name'>{plant.plant.name}</h1>
                 <div className='cart-item-value'>
                   <span className='cart-item-price'>${plant.plant.price.toFixed(2)}</span>
-                  <span className='cart-item-qty'>Qty:
-                    <select className='cart-item-qty-select' value='count'>
+                  {/* <span className='cart-item-qty'>Qty:
+                    <select onChange={this.getQty} className='cart-item-qty-select' name='qty' value={this.state.getQty}>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
                         <option value='3'>3</option>
                         <option value='4'>4</option>
                         <option value='5'>5</option>
                       </select>
-                  </span>
+                  </span> */}
                 </div>
               </div>
-              <button className='cart-item-delete' onClick={() => {this.props.removeFromCart(plant.plant)}}>X</button>
+              <span className='cart-item-delete' onClick={() => {this.props.removeFromCart(plant.plant)}}>
+                <Icon name='trash alternate outline' size='small' />
+              </span>
             </li>
           ))}
+        </div>
 
-        </div>
-        {/* <h2 className='cart-total'>Your total:  </h2> */}
-        <div className='cart-total'>
-            <span className='cart-total-label'>
-               Your Total:
-             </span>
-                 <span className='cart-total-value'>
-         
-                 </span>
-        </div>
-        <button>Checkout</button>
+          <CartTotal cart={cart}/>
+          <button type='button' className='cart-checkout-button'>Checkout</button>
       </div>
     )
   }
