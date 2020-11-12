321fc2dda99b6161ff01567370bb0eed1196ebb2
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-12 10:07:04 -0800

    fixed plant detail page styling

--- src/Components/PlantDetails.js
+++ src/Components/PlantDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Card, Icon, Button, Image} from 'semantic-ui-react'
+import {Icon, Button, Image, Grid} from 'semantic-ui-react'
 import ModalAddToCart from './ModalAddToCart'
+
 
 
 const PlantDetails= ({chosenPlant, goBackToAllPlants, cart, addToCart, addFavorite}) => {
@@ -10,32 +11,40 @@ const PlantDetails= ({chosenPlant, goBackToAllPlants, cart, addToCart, addFavori
 
   const extra = (
     <div onClick={() => addFavorite(chosenPlant)}>
-      <Icon bordered color='red' name='heart outline'  />
+      <Icon color='red' name='heart outline'  />
     </div>
   )
 
   return(
-    <Card>
-          <Image src={chosenPlant.image} alt="Plant" wrapped ui={false}/> 
-        <Card.Content>
-           <Card.Header>{chosenPlant.name}</Card.Header>
-           <Card.Description>{chosenPlant.description}</Card.Description>
-            <h4>{chosenPlant.easyToCare ? "Low Maintenance" : "High Maintenance"}</h4>
-            <h4>{chosenPlant.lowLight ? "Low Light" : "Bright Light"}</h4>
-            <h3>${price}</h3>
-            <ModalAddToCart addToCart={addToCart} chosenPlant={chosenPlant} />
-            {/* <button onClick={() => addToCart(chosenPlant)}>Add To Cart</button> */}
-            <br />
-            <br />
-            <Button onClick={goBackToAllPlants}>Back To All Plants</Button>
-            <br />
-            <br />
-            </Card.Content>
-            <div className="extra">
+    <Grid>
+        <Grid.Column width={4}>
+          <Image src={chosenPlant.image} alt="Plant"/> 
+          <br/ >
+          <div className="extra">
             {extra}
-        </div>
-    </Card>
-  )
+          </div>
+        </Grid.Column>
+        <Grid.Column width={9}>
+        <h1> {chosenPlant.name}</h1>
+          <h2>${price}</h2>
+            <p>{chosenPlant.description}</p>
+               <h4 className='care'>{chosenPlant.easyToCare ? "Low Maintenance" : "High Maintenance"}</h4>
+                 <h4 className='care'>{chosenPlant.lowLight ? "Low Light" : "Bright Light"}</h4>
+                 <br />
+                    <ModalAddToCart addToCart={addToCart} chosenPlant={chosenPlant} />
+      {/* <button onClick={() => addToCart(chosenPlant)}>Add To Cart</button> */}
+                    <br />
+                    <br />
+                    <Button className='go-back-btn' color='olive' onClick={goBackToAllPlants}>Back To All Plants</Button>
+                    <br />
+                    <br />
+        </Grid.Column>
+    </Grid>
+   
+     
+          
+)
+
 }
 
 export default PlantDetails
